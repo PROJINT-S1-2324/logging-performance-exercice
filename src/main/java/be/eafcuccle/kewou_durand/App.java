e055c90c326e4e1208fd2f04f90b1273726dcd19
kewou.durand <kewoudurand@gmail.com>
2023-10-07 14:21:38 +0200

    Mesure du temps d'exécution

--- src/main/java/be/eafcuccle/kewou_durand/App.java
+++ src/main/java/be/eafcuccle/kewou_durand/App.java
@@ -5,20 +5,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-
 public class App {
-
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
 
 
+
     public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
+
 
         for (int i = 0; i < 1_000_000; i++) {
 
             logger.debug("Message DEBUG n°{}", i);
 
         }
+
+
+
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime) / 1_000_000;  // convert to milliseconds
+
+        logger.info("Duration: {} ms", duration);
 
     }
 
